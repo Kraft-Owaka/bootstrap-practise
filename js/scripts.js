c2fba5644bf6f42bad46687771d8ed9c770a47a6
kraft-owaka <owakakraft@gmail.com>
2019-11-21 17:03:27 +0300

    added a home page and a nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
- console.log('It works')
+// contact form 
  $(documents).ready(function(){
    $('.submit').click(function(event) {
       event.preventDefault()
       console.log('Clicked button')
 
-      var email = &('.email').valueOf()
+      var email = $('.email').val()
       var subject = $('.subject').val()
       var message = $('.message').val()
+      var statusEln = $('.status')
+      statusEln.empty()
 
-      if(email.length > 5 && email.includes('e') && email.includes('.'))
-      console.log('email is valid') 
+      if(email.length > 5 && email.includes('@') && email.includes('.')) {
+        statusEln.append('<div>Email is  valid</div>')
       } else {
-      console.log('email is not valid')
+        event.preventDefault()
+        statusEln.append('<div>Email is not valid</div>')
+      }
+
+      if(subject.legth > 2) {
+        statusEln.append('<div>Subject is valid</div>')
+      } else {
+        event.preventDefault()
+        statusEln.append('<div>Subject is not valid</div>')
+      }
+      if(message.legth > 20) {
+        statusEln.append('<div>messaje is valid</div>')
+      } else {
+        event.preventDefault()
+        statusEln.append('<div>message is not valid</div>')
       }
   })
 })
